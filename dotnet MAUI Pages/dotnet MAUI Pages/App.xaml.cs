edcfefd0a93e8022061e1ce6d39639f32d250be2
victor perez ngounou <vngounou26@gmail.com>
2022-10-30 17:47:39 +0100

    🤡 Use Navigation.PushAsync to navigate to the App pages #1

--- dotnet MAUI Pages/dotnet MAUI Pages/App.xaml.cs
+++ dotnet MAUI Pages/dotnet MAUI Pages/App.xaml.cs
@@ -5,7 +5,9 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-
-		MainPage = new AppShell();
+		var navPage = new NavigationPage(new MainPage());
+		navPage.Background = Colors.Green;
+		//MainPage = new NavigationPage(new MainPage());
+		MainPage = navPage;
 	}
 }

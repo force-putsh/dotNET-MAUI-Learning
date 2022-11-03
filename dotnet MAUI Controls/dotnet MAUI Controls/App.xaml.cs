27fc9a76ba48efc10e4e1fb18d3007f1a73f141a
victor perez ngounou <vngounou26@gmail.com>
2022-11-04 00:03:12 +0100

    🥳 Initial Commit

---
+++ dotnet MAUI Controls/dotnet MAUI Controls/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace dotnet_MAUI_Controls;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		//MainPage = new AppShell();
+		MainPage = new ControlToPresentData();
+    }
+}
---
+++ dotnet MAUI Controls/dotnet MAUI Controls/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_MAUI_Controls;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}

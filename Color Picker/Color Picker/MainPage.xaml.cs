ba55294f8e455d1b1d7b621fbb079b6a2ddb8040
victor perez ngounou <vngounou26@gmail.com>
2022-11-05 18:42:32 +0100

    😎 Generate Random Color #22

--- Color Picker/Color Picker/MainPage.xaml.cs
+++ Color Picker/Color Picker/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 
 public partial class MainPage : ContentPage
 {
+	bool isRandom;
 	public MainPage()
 	{
 		InitializeComponent();
@@ -9,20 +10,34 @@ public partial class MainPage : ContentPage
 
 	private void sldRed_ValueChanged(object sender, ValueChangedEventArgs e)
 	{
-		var red = sldRed.Value;
-		var green = sldGreen.Value;
-		var blue = sldBlue.Value;
-
-		Color color = Color.FromRgb(red, green, blue);
-
-		setColor(color);
+		if (!isRandom)
+		{
+            var red = sldRed.Value;
+            var green = sldGreen.Value;
+            var blue = sldBlue.Value;
+            Color color = Color.FromRgb(red, green, blue);
+            SetColor(color);
+        }
     }
 
-	private void setColor(Color color)
+	private void SetColor(Color color)
 	{
 		btnRandom.BackgroundColor = color;
 		Container.BackgroundColor = color;
 		lblHex.Text = color.ToHex();
+	}
+
+	private void btnRandom_Clicked(object sender, EventArgs e)
+	{
+		isRandom = true;
+		var rand = new Random();
+		var color = Color.FromRgb(
+            rand.Next(0, 256), rand.Next(0, 256), rand.Next(0, 256));
+		SetColor(color);
+		sldRed.Value = color.Red;
+		sldGreen.Value = color.Green;
+		sldBlue.Value = color.Blue;
+        isRandom = false;
 	}
 }
 
